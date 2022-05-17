41a6f8034853d5d1e1913375f5051e099d6fda29
jonkahana <jonkahana1996@gmail.com>
2022-05-17 20:05:19 +0300

    adjust

--- bash_scripts/cars3d__az_id.sh
+++ bash_scripts/cars3d__az_id.sh
@@ -8,10 +8,8 @@ dataset=cars3d__az_id__train
 python -u train.py \
 --dataset=$dataset \
 --model=resnet18 \
---mode=simclr_CSI \
+--mode=sup_simclr_CSI \
 --shift_trans_type=rotation \
 --batch_size=2 \
 --one_class_idx=0 \
-
-
 
